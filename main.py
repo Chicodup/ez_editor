da3423ae8c2b4fcb536cc939d4f7de5a6fc39733
Chicodup <140813799+Chicodup@users.noreply.github.com>
2024-02-18 17:36:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import json
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
 from ui import Ui_MainWindow
-
+import json
 class Widget(QMainWindow):
     def   __init__(self):
         super().__init__()
@@ -13,7 +14,19 @@ class Widget(QMainWindow):
                 "теги":[]
             }
         }
-        self.ui.notes_list.addItems(self.notes)
+        self.ui.list_btn.addItems(self.notes)
+        self.ui.list_btn.itemClicked.connect(self.show_note)
+        self.ui.save_btn.clicked.connect(self.save_notes)
+
+    def show_note(self):
+        name = self.ui.list_btn.selectedItems()[0].text()
+        self.ui.title_edit.setText(name)
+        self.ui.text_edit.setText(self.notes[name]["текст"])
+
+    def save_notes(self):
+        self.notes[self.ui.title_edit.text()] = self.ui.text_edit.toPlainText()
+        with open("notes.json", "w", encoding="utf-8") as file:
+            json.dump(self.notes, file)
 
 app = QApplication([])
 ex = Widget()
