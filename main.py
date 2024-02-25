87c52a0e925de9057dee8b303253e2566e9ff54e
Chicodup <140813799+Chicodup@users.noreply.github.com>
2024-02-25 17:31:06 +0200

    gTSWQWS2
    t545r3

--- main.py
+++ main.py
@@ -17,17 +17,50 @@ class Widget(QMainWindow):
         self.ui.list_btn.addItems(self.notes)
         self.ui.list_btn.itemClicked.connect(self.show_note)
         self.ui.save_btn.clicked.connect(self.save_notes)
+        self.ui.create_btn.clicked.connect(self.create_note)
+        self.ui.delete_btn_2.clicked.connect(self.delete_note)
+
 
     def show_note(self):
-        name = self.ui.list_btn.selectedItems()[0].text()
-        self.ui.title_edit.setText(name)
-        self.ui.text_edit.setText(self.notes[name]["текст"])
+        self.name = self.ui.list_btn.selectedItems()[0].text()
+        self.ui.title_edit.setText(self.name)
+        self.ui.text_edit.setText(self.notes[self.name]["текст"])
 
     def save_notes(self):
-        self.notes[self.ui.title_edit.text()] = self.ui.text_edit.toPlainText()
+        self.notes[self.ui.title_edit.text()] = {
+        "текст": self.ui.text_edit.toPlainText(),
+        "теги": []
+        }
         with open("notes.json", "w", encoding="utf-8") as file:
             json.dump(self.notes, file)
+        self.ui.list_btn.clear()
+        self.ui.list_btn.addItems(self.notes)
+    def clear(self):
+        self.ui.title_edit.clear()
+        self.ui.text_edit.clear()
 
+    def create_note(self):
+        self.clear()
+    def read_notes(self):
+        try:
+            with open("notes.json", "r", encoding="utf-8") as file:
+                self.notes = json.load(file)
+        except:
+            self.notes = {
+                "перша замітка":{
+                "текст": "Це текст першої замітки",
+                "теги":[]
+            }
+            }
+    def delete_note(self):
+        # try:
+            del self.notes[self.name]
+            self.clear()
+            self.ui.list_btn.clear()
+            self.ui.list_btn.addItems(self.notes)
+            self.save_notes()
+        # except:
+            # print("помилка видалення")
 app = QApplication([])
 ex = Widget()
 ex.show()
